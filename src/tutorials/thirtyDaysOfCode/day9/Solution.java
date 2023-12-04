1a78df19843a1255ba1e5c079d960866e4cd6cec
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2023-12-04 11:15:05 +0600

    Day 9 of 30 days of code solved

---
+++ src/tutorials/thirtyDaysOfCode/day9/Solution.java
@@ -0,0 +1,36 @@
+package tutorials.thirtyDaysOfCode.day9;
+
+import java.io.*;
+
+class Result {
+
+    /*
+     * Complete the 'factorial' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int factorial(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.factorial(n);
+
+        System.out.println(String.valueOf(result));
+
+        bufferedReader.close();
+    }
+}
