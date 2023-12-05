eb32beb9b9b3251ec6e8035b3bcb7a1a8d53f84b
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2023-12-05 17:32:08 +0600

    Day 10 of 30 days of Code completed

---
+++ src/tutorials/thirtyDaysOfCode/day10/Solution.java
@@ -0,0 +1,26 @@
+package tutorials.thirtyDaysOfCode.day10;
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bufferedReader.close();
+
+        String binaryString = Integer.toBinaryString(n);
+
+        int maxCount = 0, currentCount = 0;
+        for (int i = 0; i < binaryString.length(); i++) {
+            if (binaryString.charAt(i) == '1') {
+                currentCount++;
+                maxCount = Math.max(maxCount, currentCount);
+            } else {
+                currentCount = 0;
+            }
+        }
+        System.out.println(maxCount);
+    }
+}
