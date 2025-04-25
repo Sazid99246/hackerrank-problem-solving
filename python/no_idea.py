d0cfcdda85e89ffd58cb54e3f9928f520e3c5405
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-04-25 11:51:23 +0600

    symmetric difference problem solved

--- python/no_idea.py
+++ python/no_idea.py
@@ -1,2 +1,12 @@
 n, m = map(int, input().split())
-print(n, m)
+arr = list(map(int, input().split()))
+arr2 = set(map(int, input().split()))
+arr3 = set(map(int, input().split()))
+
+happiness = 0
+for i in arr:
+    if i in arr2:
+        happiness += 1
+    if i in arr3:
+        happiness -= 1
+print(happiness)
